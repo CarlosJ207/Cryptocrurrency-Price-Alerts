363fb8aa66bb0e92d7c8a73fa3e3bbf079fcdbde
JuradoC <86178860+CarlosJ207@users.noreply.github.com>
2021-12-29 06:31:25 +0000

    Added code to scrape the percent change from the website

--- main.py
+++ main.py
@@ -20,7 +20,11 @@ def main():
   driver.find_element(by = "xpath", value = "/html/body/div/div/div/section/table/tbody/tr[25]/td[1]/div/span[3]/a").click()
   time.sleep(2)
   print(driver.current_url)
- 
+  while True:
+    time.sleep(2)
+    element = driver.find_element(by = 'xpath', value = "/html/body/div/div/div/div[3]/section/div[4]/div/div/table/tbody/tr/td[2]/div")
+    text =  str(clean_txt(element.text))
+    
 
 
 print(main())
