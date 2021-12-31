f49e5a215171ecb7bdd41ca477fad25367de8ded
JuradoC <86178860+CarlosJ207@users.noreply.github.com>
2021-12-31 00:52:28 +0000

    Generalized the code to any cryptocurrency when it was initally fixed for Alogrand.

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def get_driver():
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
   driver = webdriver.Chrome(options=options)
-  driver.get("https://coinranking.com/coin/TpHE2IShQw-sJ+algorand-algo")
+  driver.get("https://coinranking.com/")
   return driver
 
 def float_txt(value):
- """Change the 24Hr percentage change from a string to a float object"""
+ #Change the 24Hr percentage change from a string to a float object
  float_value = value.replace('%', '') 
  output = float(float_value)
  return output 
@@ -24,6 +24,14 @@ def float_txt(value):
 
 def main():
   driver = get_driver()
+  #Click search button
+  search_button = driver.find_element(by = 'xpath', value = "//*[@id='__layout']/div/header/div[2]/div[1]/button")
+  search_button.click()
+
+  #Type text into the search button
+  val = input("Enter the name of the cryptocurrenct to monitor:")
+  text_area = driver.find_element(by = 'xpath', value = "//*[@id='__layout']/div/header/div[2]/div[1]/div[1]/div[2]/input")
+  text_area.send_keys(val + Keys.RETURN)
   while True:
     time.sleep(2)
     element = driver.find_element(by = 'xpath', value = "/html/body/div/div/div/div[3]/section/div[4]/div/div/table/tbody/tr/td[2]/div")
